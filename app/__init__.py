fffa96e04d8bde9bbe122c81ab0604650275d61e
tivui <tivui64@gmail.com>
2020-11-01 12:21:42 +0100

    Changement de la configuration des fichiers statiques

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware',]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS= (os.path.join(PROJECT_ROOT,'static'),)
+
 from app import views
 
