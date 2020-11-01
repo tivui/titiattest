2b73793e73bcfb365f0f812a5e84873870d24ceb
tivui <tivui64@gmail.com>
2020-11-01 13:34:13 +0100

    Mise à jour de app et requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware',]
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS= (os.path.join(PROJECT_ROOT,'static'),)
+STATICFILES_DIRS= (os.path.join(BASE_DIR,'static'),)
 
 from app import views
 
